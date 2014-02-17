34a550d278bca249d4b1990a9210ed41158d0bfd
dcods22 <daniel.cody2@marist.edu>
2014-02-17 01:26:18 -0500

    Added basic logic

--- src/charlie/advisor/Advisor.java
+++ src/charlie/advisor/Advisor.java
@@ -9,14 +9,33 @@ import charlie.card.Card;
 import charlie.card.Hand;
 import charlie.plugin.IAdvisor;
 import charlie.util.Play;
+
 /**
- *
+ * Class to give advice on what Black Jack move to make
  * @author Dan
  */
+
 public class Advisor implements IAdvisor{
+    
     @Override
-    public Play advise(Hand myHand,Card upCard){
-        Play advice = Play.NONE;
+    public Play advise(Hand myHand, Card upCard){
+        Card cardOne = myHand.getCard(0);
+        Card cardTwo = myHand.getCard(1);
+        Play advice;
+        
+        if(myHand.isPair()){
+           //pair hash table
+            advice = Play.NONE;
+        }
+        else if(cardOne.isAce() || cardTwo.isAce()){
+            //ace hash table
+            advice = Play.NONE;
+        }else{
+            //total number hash table
+            advice = Play.NONE;
+        }
+            
         return advice;
     };
+    
 }
